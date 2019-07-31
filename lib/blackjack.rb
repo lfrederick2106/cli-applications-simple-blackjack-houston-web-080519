c89c190e70e5f3be89bb0ef545affc8a705e06f5
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-31 04:30:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,7 +24,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  puts display_card_total(deal_card + deal_card.sum)
+  n = (deal_card + deal_card.sum)
+  puts display_card_total(n)
   return initial_round
 end
 
